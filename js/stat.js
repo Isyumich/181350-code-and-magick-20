1747f1e873fa13616ee9d550c64c38fa33c2553e
isyumich <59385945+Isyumich@users.noreply.github.com>
2020-05-31 17:53:17 +0300

    Исправление задания

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Константы
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -9,12 +9,13 @@ var ELEMENT_WIDTH = 40;
 var ELEMENT_GAP = 50;
 var MAX_ELEMENT_HEIGHT = 150;
 
-
+// Функция создания облака
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+// Функция получения максимального времени среди игроков
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 0; i < arr.length; i++) {
@@ -25,10 +26,26 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+// Функция получения случайной насыщенности цвета
 function getRandomSaturate(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+// Функция получения статистики игроков
+function getPlayersStatistic(ctx, players, times) {
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < times.length; i++) {
+    var blueColor = getRandomSaturate(0, 100);
+    ctx.fillStyle = (players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + blueColor + '%, 50%)');
+    var elementHeight = times[i] * MAX_ELEMENT_HEIGHT / maxTime;
+    ctx.fillText(players[i], CLOUD_X + 40 + (ELEMENT_WIDTH + ELEMENT_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - 20);
+    ctx.fillRect(CLOUD_X + 40 + (ELEMENT_WIDTH + ELEMENT_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - 30, ELEMENT_WIDTH, -elementHeight);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + 40 + (ELEMENT_WIDTH + ELEMENT_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - elementHeight - 50);
+  }
+}
+
+// Функция отрисовки статистики игры
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -39,14 +56,5 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 20);
   ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 40);
 
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < times.length; i++) {
-    var blueColor = getRandomSaturate(0, 100);
-    ctx.fillStyle = (players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + blueColor + '%, 50%)');
-    var elementHeight = times[i] * MAX_ELEMENT_HEIGHT / maxTime;
-    ctx.fillText(players[i], CLOUD_X + 40 + (ELEMENT_WIDTH + ELEMENT_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - 20);
-    ctx.fillRect(CLOUD_X + 40 + (ELEMENT_WIDTH + ELEMENT_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - 30, ELEMENT_WIDTH, -elementHeight);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + 40 + (ELEMENT_WIDTH + ELEMENT_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - MAX_ELEMENT_HEIGHT - 50);
-  }
+  getPlayersStatistic(ctx, players, times);
 };
